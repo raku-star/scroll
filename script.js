3d45831e688fa06214b389a5971ce3098014a743
raku-star <79367241+raku-star@users.noreply.github.com>
2021-02-28 00:49:38 +0900

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,4 @@
 const targetElement = document.querySelectorAll(".animationTarget");
-console.log("画面の高さ",window.innerHeight)
 document.addEventListener("scroll",function(){
     for (let i = 0; i < targetElement.length; i++) {
         const getElementDistance = targetElement[i].
